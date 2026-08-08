ddc8737932b70281cf7b81b24032e94cb69feb7b
Aniket-1504 <161802407+Aniket-1504@users.noreply.github.com>
2026-08-08 20:55:43 +0530

    Add: non-cyclical-number - submission-0

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isHappy(int n) {
+        for(int i=0;i<1000;i++){
+            if(n==1){
+                return true;
+            }
+            n=sumOfSquaredDigits(n);
+        }
+        return false;
+    }
+        private int sumOfSquaredDigits(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+}
+}
